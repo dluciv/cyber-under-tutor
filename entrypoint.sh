1e03f15240478428b5238f7abe2a948143398140
Dmitry V. Luciv <dluciv@dluciv.name>
2022-01-11 21:26:09 +0300

    Trying reusable workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,6 @@
 
 echo "::group::Кибердядька работает"
 
-UNDERTUTOR_BRANCH=cyber-under-tutor
-
 LINES_PER_MSG=$1
 
 count_lines() {
@@ -17,27 +15,9 @@ cppcheck $CPPCHECK_OPTS       --output-file=cppcheck.log .
 cppcheck $CPPCHECK_OPTS --xml --output-file=cppcheck.xml .
 CCRES=$?
 
-# -----------------------------
-MAIN_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-git config pull.rebase false
-git config user.email "CYBER@DRDbKA.github.com"
-git config user.name "Кибердядька"
-git pull origin $UNDERTUTOR_BRANCH
-git switch $UNDERTUTOR_BRANCH
-
 echo "::endgroup::"
 
 echo $CCXML | $(dirname "$0")/c-check.py quality-check $CC_LINES $LINES_PER_MSG cppcheck.xml cppcheck.log
-
-echo "::group::Кибердядька работает"
-
-git add quality-check.yml quality-check.svg
-git commit -m "Кибердядька сообщает"
-git push origin $UNDERTUTOR_BRANCH
-git switch $MAIN_BRANCH
-# -----------------------------
-
-echo "::endgroup::"
 
 echo "::group::Кибердядька сообщает"
 echo cppcheck status:
