62b377ef73b93771f3284566476524dfba75409a
Dmitry V. Luciv <dluciv@dluciv.name>
2022-01-11 12:50:25 +0300

    Padding

--- c-check.py
+++ c-check.py
@@ -17,5 +17,5 @@ score_promille = str(round(score * 1000))
 with open(sys.argv[2] + '.yml', 'w', encoding='utf-8' ) as y:
   yaml.dump(report, y)
 
-badge = anybadge.Badge('Кибердядька', score_promille, thresholds={0: 'red', 500: 'orange', 750: 'yellow', 900: 'green'}, value_suffix='‰')
+badge = anybadge.Badge('Кибердядька', score_promille, thresholds={0: 'red', 500: 'orange', 750: 'yellow', 900: 'green'}, value_suffix='‰', num_padding_chars=1)
 badge.write_badge(sys.argv[2] + '.svg')
